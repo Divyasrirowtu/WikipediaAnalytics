af6a90047ee1be8d7036a790ba9df07ff14a3821
Divyasrirowtu <divyasrirowtu@gmail.com>
2026-07-30 19:53:14 +0530

    Implement Wikipedia XML data ingestion using spark-xml

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
+import os
+from dotenv import load_dotenv
+
 from spark_session import create_spark_session
+from ingestion import read_wikipedia_xml
 
 
 def main():
 
+    load_dotenv(".env.example")
+
     spark = create_spark_session()
 
-    print("=" * 50)
-    print("Spark Started Successfully")
-    print("=" * 50)
+    input_path = os.getenv("INPUT_PATH")
+
+    df = read_wikipedia_xml(spark, input_path)
+
+    print("\n===== SCHEMA =====")
+    df.printSchema()
+
+    print("\n===== SAMPLE DATA =====")
+    df.show(5, truncate=False)
 
     spark.stop()
 
